b4e7d62aeabeac7aec09e3d68c32e1a6a5e84036
saleel <saleel@saleel.xyz>
2025-04-22 17:37:36 +0000

    latest bb

--- js/generate-proof.ts
+++ js/generate-proof.ts
@@ -1,4 +1,4 @@
-import { Barretenberg, RawBuffer, UltraHonkBackend } from "@aztec/bb.js";
+import { UltraHonkBackend } from "@aztec/bb.js";
 import innerCircuit from "../circuits/inner/target/inner.json" assert { type: "json" };
 import recursiveCircuit from "../circuits/recursive/target/recursive.json" assert { type: "json" };
 import { CompiledCircuit, Noir } from "@noir-lang/noir_js";
@@ -11,25 +11,24 @@ import { CompiledCircuit, Noir } from "@noir-lang/noir_js";
     // Generate proof for inner circuit
     const inputs = { x: 3, y: 3 }
     const { witness } = await innerCircuitNoir.execute(inputs);
-    const { proof: innerProofFields, publicInputs: innerPublicInputs } = await innerBackend.generateProofForRecursiveAggregation(witness);
-
-    // Get verification key for inner circuit as fields
-    const innerCircuitVerificationKey = await innerBackend.getVerificationKey();
-    const barretenbergAPI = await Barretenberg.new({ threads: 1 });
-    const vkAsFields = (await barretenbergAPI.acirVkAsFieldsUltraHonk(new RawBuffer(innerCircuitVerificationKey))).map(field => field.toString());
+    const { proof, publicInputs } = await innerBackend.generateProof(witness);
+    const { proofAsFields, vkAsFields } = await innerBackend.generateRecursiveProofArtifacts(
+      proof,
+      publicInputs
+    );
 
     // Generate proof of the recursive circuit
     const recursiveCircuitNoir = new Noir(recursiveCircuit as CompiledCircuit);
     const recursiveBackend = new UltraHonkBackend(recursiveCircuit.bytecode, { threads: 1 });
 
-    const recursiveInputs = { proof: innerProofFields, public_inputs: innerPublicInputs, verification_key: vkAsFields };
+    const recursiveInputs = { proof: proofAsFields, public_inputs: publicInputs, verification_key: vkAsFields };
     const { witness: recursiveWitness } = await recursiveCircuitNoir.execute(recursiveInputs);
     const { proof: recursiveProof, publicInputs: recursivePublicInputs } = await recursiveBackend.generateProof(recursiveWitness);
 
     // Verify recursive proof
     const verified = await recursiveBackend.verifyProof({ proof: recursiveProof, publicInputs: recursivePublicInputs });
     console.log("Recursive proof verified: ", verified);
-    
+
     process.exit(verified ? 0 : 1);
   } catch (error) {
     console.error(error);
